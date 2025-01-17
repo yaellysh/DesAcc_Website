14e703fe6f11e666f22b2beb924c34eb82434536
Yael Lyshkow <yaellyshkow@outlook.com>
2025-01-17 10:58:54 -0500

    changed logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => (
     <div className="flex items-center">
       <a href="/" className="flex items-center">
         <img src="/Whoosh Teal (RGB).png" alt="Icon" className="mx-2 md:mx-4 h-[2rem] md:h-[3rem]" />
-        <img src="/DesAcc_logo_rgb_horizontal_with line_delft blue4.png" alt="Desacc title" className="h-[1.5rem] md:h-[3rem]" />
+        <img src="/DesAcc_logo.png" alt="Desacc title" className="h-[1.5rem] md:h-[3rem]" />
       </a>
     </div>
   </header>
